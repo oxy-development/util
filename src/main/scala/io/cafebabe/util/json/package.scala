d2c497a0439361d63b0a9e5aa0767caaa765a6c3
Vladimir Konstantinov <illfaku@gmail.com>
2015-06-10 09:14:36 +0600

    added json default formats

---
+++ src/main/scala/io/cafebabe/util/json/package.scala
@@ -0,0 +1,11 @@
+package io.cafebabe.util
+
+import org.json4s.DefaultFormats
+
+/**
+ * @author Vladimir Konstantinov
+ * @version 1.0 (4/23/2015)
+ */
+package object json {
+  implicit val formats = DefaultFormats
+}
